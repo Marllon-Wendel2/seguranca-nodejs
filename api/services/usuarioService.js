8d684d901bf7221d292aa9b20f378bc235e0ba7a
Marllon Wendel <m.w.p.ferreira@outlook.com>
2024-04-12 12:29:24 -0300

    post

---
+++ api/services/usuarioService.js
@@ -0,0 +1,32 @@
+const db = require('../models')
+const { hash } = require('bcryptjs')
+const uuid = require('uuid')
+
+class UsuarioService {
+    async cadastrar(dto) {
+        const usuario = await db.usuarios.findOne({
+            where: {
+                email: dto.email
+            }
+        })
+
+        if (usuario) {
+            throw new Error('Usuario ja cadastrado')
+        }
+
+        try { 
+            const senhaHash = await hash(dto.senha, 8)
+    
+            const novoUsuario = await database.usuarios.create({
+                id: uuid.v4(),
+                nome: dto.nome,
+                email: dto.email,
+                senha: senhaHash
+            })
+    
+            return novoUsuario
+        } catch (error) {
+            throw new Error('Erro ao cadastrar usuario')
+        }}}
+
+module.exports = UsuarioService
